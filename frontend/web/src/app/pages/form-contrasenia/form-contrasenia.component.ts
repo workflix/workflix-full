bf7014cab8618e35ab365ed67aa17e5e7b0788dd
Yandira Diaz <yandiradiaz@hotmail.com>
2024-04-27 00:00:11 -0300

    se aplican estilos a formulario

--- frontend/web/src/app/pages/form-contrasenia/form-contrasenia.component.ts
+++ frontend/web/src/app/pages/form-contrasenia/form-contrasenia.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-contrasenia',
@@ -10,12 +11,16 @@ export class FormContraseniaComponent {
 
   myForm: FormGroup
   //private fb:FormBuilder = new FormBuilder()
-  constructor (private fb:FormBuilder){
+  constructor (private fb:FormBuilder, private router: Router){
     this.myForm = this.fb.group({
       usuario: ['']
     });
   }
   reset(){
     console.log(this.myForm.value) // imprime en consola lo que completó el usuario
+  }
+
+  volver(){
+    this.router.navigate(['/login'])
   }
 }
