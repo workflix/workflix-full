17044208f0c3ea5b3cca3a48224c75324c0e8c41
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2024-04-30 01:14:45 -0300

    [ADD]ruta ingresar en app.routes.ts

--- frontend/web/3.0.00/src/app/app.routes.ts
+++ frontend/web/3.0.00/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './guards/auth.guard';
+import { IngresarComponent } from './pages/ingresar/ingresar.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'ingresar', component: IngresarComponent },
 ];
