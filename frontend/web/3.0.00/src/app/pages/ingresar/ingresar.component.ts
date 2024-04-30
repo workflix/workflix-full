f0c42828db09dbcd927e38ae564efecd7fa51e4b
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2024-04-30 00:50:11 -0300

    [ADD]ingresar.ts

--- frontend/web/3.0.00/src/app/pages/ingresar/ingresar.component.ts
+++ frontend/web/3.0.00/src/app/pages/ingresar/ingresar.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
+import { FormBuilder, MinValidator, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { HttpStatusCode } from '@angular/common/http';
+import {  OnInit, ElementRef, Input } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { subscribeOn } from 'rxjs';
+/* 
+import { ResultadoApi } from 'src/app/models/modelo.resultado';
+import { AuthService } from 'src/app/services/auth.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
+ */
 @Component({
   selector: 'app-ingresar',
   standalone: true,
