0ae959682119c60174b4cfa51b7c7cf63ea1ecaa
Braian <troncosobraianaxel@gmail.com>
2024-04-30 10:26:12 -0300

    [ADD] validaciones guard

--- frontend/web/3.0.00/src/app/guards/auth.guard.ts
+++ frontend/web/3.0.00/src/app/guards/auth.guard.ts
@@ -6,5 +6,10 @@ import { LoginService } from '../services/login.service';
 export const authGuard: CanActivateFn = ( route,state ) => {
   const loginService = inject(LoginService);
   const router = inject(Router);
-
+if(loginService.isLoggedIn()){
+  return true;
+}else{
+  const url = router.createUrlTree(['/login']);
+  return url;
+}
 };
