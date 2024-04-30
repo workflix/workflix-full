a806ff8afe32cfe4903e9e2b535ba308c7978fe3
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2024-04-30 00:47:36 -0300

    [ADD]modulo ingresar

---
+++ frontend/web/3.0.00/src/app/pages/ingresar/ingresar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingresar',
+  standalone: true,
+  imports: [],
+  templateUrl: './ingresar.component.html',
+  styleUrl: './ingresar.component.css'
+})
+export class IngresarComponent {
+
+}
