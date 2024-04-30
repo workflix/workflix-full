b138ad97eed0ea677ea12c7dc17eeb883d22a0c1
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2024-04-30 18:45:06 -0300

    [ADD] componenete registrarse

---
+++ frontend/web/3.0.00/src/app/pages/registrarse/registrarse.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrarse',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrarse.component.html',
+  styleUrl: './registrarse.component.css'
+})
+export class RegistrarseComponent {
+
+}
