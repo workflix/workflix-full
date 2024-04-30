5d731fdc1b4af01910f8096cfe6c19a472e1016e
Braian <troncosobraianaxel@gmail.com>
2024-04-30 16:13:49 -0300

    [ADD] vinculacion api front a back

--- frontend/web/3.0.00/src/app/services/register.service.ts
+++ frontend/web/3.0.00/src/app/services/register.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { User } from '../models/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
+  private url = 'http://localhost:8080/api/v1/user/register';
+
+ 
 }
