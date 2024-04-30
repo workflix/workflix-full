1d810c43729437ac95e0086f8375ce4e6b11140a
Braian <troncosobraianaxel@gmail.com>
2024-04-30 10:28:22 -0300

    [UPDATE] routes validacion guard home

--- frontend/web/3.0.00/src/app/app.routes.ts
+++ frontend/web/3.0.00/src/app/app.routes.ts
@@ -3,4 +3,5 @@ import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: 'home', component: AcavElComponenteQueRepresenteHine, canActivate: [authGuard]}, No habilitar guard hasta que esté register
 ];
