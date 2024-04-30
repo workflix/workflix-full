f746d13de09578b6aa56b3e9672131af713899e1
Braian <troncosobraianaxel@gmail.com>
2024-04-30 10:26:51 -0300

    [FIX] guard routes rename

--- frontend/web/3.0.00/src/app/app.routes.ts
+++ frontend/web/3.0.00/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { AuthGuard } from './guards/auth.guard';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
