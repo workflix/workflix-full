996cfe3148fcd5d8724df9d4199ba109aecc01c7
Braian <troncosobraianaxel@gmail.com>
2024-04-30 10:25:53 -0300

    [ADD] inyect instancias

--- frontend/web/3.0.00/src/app/guards/auth.guard.ts
+++ frontend/web/3.0.00/src/app/guards/auth.guard.ts
@@ -6,10 +6,5 @@ import { LoginService } from '../services/login.service';
 export const authGuard: CanActivateFn = ( route,state ) => {
   const loginService = inject(LoginService);
   const router = inject(Router);
-if(loginService.isLoggedIn()){
-  return true;
-}else{
-  const url = router.createUrlTree(['/login']);
-  return url;
-}
+
 };
