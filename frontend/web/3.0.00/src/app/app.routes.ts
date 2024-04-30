4a5852a3a400493645ce59347a5161635ebf980e
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2024-04-30 18:57:04 -0300

    [ADD] ruta registrarse

--- frontend/web/3.0.00/src/app/app.routes.ts
+++ frontend/web/3.0.00/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { IngresarComponent } from './pages/ingresar/ingresar.component';
 import { authGuard } from './guards/auth.guard';
+import { RegistrarseComponent } from './pages/registrarse/registrarse.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   // { path: 'home', component: AcavElComponenteQueRepresenteHine, canActivate: [authGuard]}, No habilitar guard hasta que esté register
   { path: 'ingresar', component: IngresarComponent },
+  { path: 'registrase', component: RegistrarseComponent },
 ];
