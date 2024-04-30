d593cdad99bdc95c8b64dce46e31421978770b93
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2024-04-30 01:13:32 -0300

    modifica ingresar.ts

--- frontend/web/3.0.00/src/app/pages/ingresar/ingresar.component.ts
+++ frontend/web/3.0.00/src/app/pages/ingresar/ingresar.component.ts
@@ -18,6 +18,108 @@ import { UsuariosService } from 'src/app/services/usuarios.service';
   templateUrl: './ingresar.component.html',
   styleUrl: './ingresar.component.css'
 })
-export class IngresarComponent {
+export class IngresarComponent /* implements OnInit */ {
+  /* loginForm!: FormGroup;
+  usuario;
+  @Input() resultado: ResultadoApi | undefined;
+
+  
+  forma!:FormGroup; // Declaración de la variable 'forma'
+  
+//------------------------------------------------------------------------------------
+  get usuarioNoValido(){
+
+
+    return this.forma.get('user')?.invalid && this.forma.get('user')?.touched;
+
+  }
+
+  
+
+  get passwordNoValido(){
+
+    return this.forma.get('password')?.invalid && this.forma.get('password')?.touched;
+
+  }
+
+  
+
+//-----------------------------------------------------------------------------------
+
+constructor(
+  private fb: FormBuilder,
+  private router: Router,
+  private elementRef: ElementRef,
+  private authService: AuthService
+) {
+  this.usuario = { user: "", password: "" };
+  this.resultado = undefined;
+  this.crearFormulario();
+}
+
+
+
+  ngOnInit(): void {
+      this.loginForm = this.fb.group({
+        user: [this.usuario.user, [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+        password: [this.usuario.password, [Validators.required, Validators.minLength(6), Validators.maxLength(20)]],
+    });
+  }
+    
+
+  crearFormulario(){
+    this.forma = this.fb.group({
+      user:['', [Validators.required , Validators.minLength(4)]],
+      
+      
+      password:['', [Validators.required , Validators.minLength(6)]],
+     
+
+    }, {
+      
+      
+    }
+    
+    )
+  }
+
+  get user() { return this.loginForm.get('user'); }
+  get password() { return this.loginForm.get('password'); }
+
+  onSubmit(value: any) {
+  this.authService.login(value.user, value.password)
+    .subscribe({
+      next: (exito: ResultadoApi) => {
+        this.resultado = undefined;
+        this.router.navigate(['/']);
+        this.elementRef.nativeElement.ownerDocument.documentElement.scrollTop = 0;
+      },
+      error: (error: ResultadoApi) => {
+        this.resultado = error;
+      },
+      complete: () => {}
+    });
+}
+
+  limpiar(){
+
+  this.forma.reset();
+
+  }
+
+  guardar() {
+    console.log(this.forma);
+  
+    if (this.forma.invalid) {
+      return Object.values(this.forma.controls).forEach((control) => {
+        control.markAllAsTouched();
+      });
+    }
+  
+    const value = this.forma.value;
+    this.onSubmit(value);
+  }
+ */
+ 
 
 }
