702c9656634d2d6e74f5052a6e3b873e7a03b7dc
marceloedelgado <marceloedelgado@gmail.com>
2024-05-01 00:56:00 -0300

    [FIX] imports HomeComponent

--- frontend/web/3.0.00/src/app/app.component.ts
+++ frontend/web/3.0.00/src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components//footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, FooterComponent, ],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, HomeComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'servicio-libre-web';
+  title = 'Workflix';
 }
