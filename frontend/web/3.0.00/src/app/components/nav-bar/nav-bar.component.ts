b56f00394ff38bcfe2332bd2ed48a5025cbca05c
Yandira Diaz <yandiradiaz@hotmail.com>
2024-05-01 10:15:37 -0300

    [ADD] imports del nav-bar

--- frontend/web/3.0.00/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/web/3.0.00/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { CarritoService } from 'src/app/services/carrito.service';
+import { ScrollingService } from 'src/app/scrolling.service';
+import { HttpStatusCode } from '@angular/common/http';
+
+import { Router } from '@angular/router';
+import { ResultadoApi } from 'src/app/models/modelo.resultado';
+import { Usuario, TipoUsuario } from 'src/app/models/modelo.usuario';
+import { AuthService } from 'src/app/services/auth.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-nav-bar',
