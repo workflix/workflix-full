86e6ecec08c0e8bd9cdffbca137210fc1ba1daca
marceloedelgado <marceloedelgado@gmail.com>
2024-05-01 00:57:01 -0300

    [ADD] routing al home

--- frontend/web/3.0.00/src/app/app.routes.ts
+++ frontend/web/3.0.00/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './guards/auth.guard';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
