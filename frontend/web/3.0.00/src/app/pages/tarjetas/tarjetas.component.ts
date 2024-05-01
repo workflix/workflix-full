117c9e2d7eaf94e679d4611fc49e261ce3e93533
marceloedelgado <marceloedelgado@gmail.com>
2024-05-01 17:41:57 -0300

    [FIX] standalone: true '

--- frontend/web/3.0.00/src/app/pages/tarjetas/tarjetas.component.ts
+++ frontend/web/3.0.00/src/app/pages/tarjetas/tarjetas.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tarjetas',
   standalone: true,
-  imports: [],
   templateUrl: './tarjetas.component.html',
   styleUrl: './tarjetas.component.css'
 })
