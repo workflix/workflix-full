0eac5d55b2905b26d8999c44ce3a354ce5fe75e2
marceloedelgado <marceloedelgado@gmail.com>
2024-05-01 17:46:54 -0300

    [ADD] Se agrega path componente TarjetasComponent

--- frontend/web/3.0.00/src/app/app.routes.ts
+++ frontend/web/3.0.00/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { IngresarComponent } from './pages/ingresar/ingresar.component';
 import { authGuard } from './guards/auth.guard';
+import { TarjetasComponent } from './pages/tarjetas/tarjetas.component';
 
 
 export const routes: Routes = [
@@ -11,4 +12,5 @@ export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   // { path: 'home', component: AcavElComponenteQueRepresenteHine, canActivate: [authGuard]}, No habilitar guard hasta que esté register
   { path: 'ingresar', component: IngresarComponent },
+  { path: 'tarjetas', component: TarjetasComponent }
 ];
