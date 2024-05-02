b8903109b84188b090bb2908196bf4118069f8af
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2024-05-02 16:02:02 -0300

    [ADD] ruta a ingresar en footer

--- frontend/web/3.0.00/src/app/components/footer/footer.component.ts
+++ frontend/web/3.0.00/src/app/components/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ContactoComponent } from '../../pages/contacto/contacto.component';
+import { IngresarComponent} from '../../pages/ingresar/ingresar.component';
 
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [ContactoComponent],
+  imports: [RouterModule,ContactoComponent,IngresarComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
