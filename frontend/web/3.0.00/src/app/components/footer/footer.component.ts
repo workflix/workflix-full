a85b2b9456d7a23c237aac4d5173cf34d58751ed
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2024-05-02 16:22:51 -0300

    [ADD] ruta a registrarse en footer

--- frontend/web/3.0.00/src/app/components/footer/footer.component.ts
+++ frontend/web/3.0.00/src/app/components/footer/footer.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { ContactoComponent } from '../../pages/contacto/contacto.component';
 import { IngresarComponent} from '../../pages/ingresar/ingresar.component';
-
 import { RouterModule } from '@angular/router';
+import { RegistrarseComponent } from '../../pages/registrarse/registrarse.component'
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule,ContactoComponent,IngresarComponent],
+  imports: [RouterModule,ContactoComponent,IngresarComponent,RegistrarseComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
