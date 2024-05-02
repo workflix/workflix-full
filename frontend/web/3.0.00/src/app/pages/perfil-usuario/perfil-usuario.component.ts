4789d34ab6f80eb31175af6ad09d4a12559a39b9
Braian <troncosobraianaxel@gmail.com>
2024-05-01 22:44:33 -0300

    [ADD] componente perfil-usuario

---
+++ frontend/web/3.0.00/src/app/pages/perfil-usuario/perfil-usuario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfil-usuario.component.html',
+  styleUrl: './perfil-usuario.component.css'
+})
+export class PerfilUsuarioComponent {
+
+}
