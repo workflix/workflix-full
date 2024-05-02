815abcbc90d4a827f41a6cb3196055fd64bc9395
NataliaAlvarezIspc <natalia.humanista@gmail.com>
2024-05-02 15:09:37 -0300

    [ADD] ruta contacto en app.routes

--- frontend/web/3.0.00/src/app/app.routes.ts
+++ frontend/web/3.0.00/src/app/app.routes.ts
@@ -7,16 +7,18 @@ import { TarjetasComponent } from './pages/tarjetas/tarjetas.component';
 import { RegistrarseComponent } from './pages/registrarse/registrarse.component';
 import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
 import { PerfilUsuarioComponent } from './pages/perfil-usuario/perfil-usuario.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   // { path: 'home', component: AcavElComponenteQueRepresenteHine, canActivate: [authGuard]}, No habilitar guard hasta que esté register
+  { path: 'registrarse', component: RegistrarseComponent },
   { path: 'ingresar', component: IngresarComponent },
   { path: 'tarjetas', component: TarjetasComponent },
-  { path: 'registrarse', component: RegistrarseComponent },
   { path: 'quienes-somos', component: QuienesSomosComponent },
-  { path: 'perfil-usuario', component: PerfilUsuarioComponent }
+  { path: 'perfil-usuario', component: PerfilUsuarioComponent },
+  { path: 'contacto', component: ContactoComponent },
 
 ];
