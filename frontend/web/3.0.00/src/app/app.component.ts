2faae6e6f1eaf73b5d76b92c18d30af430e1cdce
marceloedelgado <marceloedelgado@gmail.com>
2024-05-03 19:43:24 -0300

    [ADD] importacion de RecuperarClaveComponent

--- frontend/web/3.0.00/src/app/app.component.ts
+++ frontend/web/3.0.00/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components//footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { TarjetasComponent } from './pages/tarjetas/tarjetas.component';
+import { RecuperarClaveComponent } from './pages/recuperar-clave/recuperar-clave.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, FooterComponent, HomeComponent, TarjetasComponent ],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent, HomeComponent, TarjetasComponent, RecuperarClaveComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
