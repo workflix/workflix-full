cfada6b3be908bcdf1fcbbb5645826a28474abc0
marceloedelgado <marceloedelgado@gmail.com>
2024-05-04 12:56:24 -0300

    [ADD] CrossOrigin en loginApiController, RegisterApiController y UsuarioController

--- backend/spring/src/main/java/com/tec/workflix/controllers/LoginApiController.java
+++ backend/spring/src/main/java/com/tec/workflix/controllers/LoginApiController.java
@@ -10,8 +10,8 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 @RestController
+
 @CrossOrigin(origins = "http://localhost:4200/")
 @RequestMapping("/api/v1")
 public class LoginApiController {
