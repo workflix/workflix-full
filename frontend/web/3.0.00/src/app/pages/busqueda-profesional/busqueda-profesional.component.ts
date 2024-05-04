8bc672e260a4a4511029da8ba04aaae1ee5ff3ea
Yandira Diaz <yandiradiaz@hotmail.com>
2024-05-04 17:50:05 -0300

    [ADD] busqueda-profesional

---
+++ frontend/web/3.0.00/src/app/pages/busqueda-profesional/busqueda-profesional.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda-profesional',
+  standalone: true,
+  imports: [],
+  templateUrl: './busqueda-profesional.component.html',
+  styleUrl: './busqueda-profesional.component.css'
+})
+export class BusquedaProfesionalComponent {
+
+}
