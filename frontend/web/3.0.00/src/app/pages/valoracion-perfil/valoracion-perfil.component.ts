3bfb15318f89b032934030d17b642fde2afcd7ae
Valentino Lorenti <lorentivalentino@gmail.com>
2024-05-04 15:49:22 -0300

    [ADD] Componente valoracion-perfil

---
+++ frontend/web/3.0.00/src/app/pages/valoracion-perfil/valoracion-perfil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-valoracion-perfil',
+  standalone: true,
+  imports: [],
+  templateUrl: './valoracion-perfil.component.html',
+  styleUrl: './valoracion-perfil.component.css'
+})
+export class ValoracionPerfilComponent {
+
+}
