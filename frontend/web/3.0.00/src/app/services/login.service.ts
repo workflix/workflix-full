120ff7e46c732ee803657e6090d936b39a90907a
Braian <troncosobraianaxel@gmail.com>
2024-05-04 22:02:30 -0300

    [UPDATE] loginservice - behaviorsubject

--- frontend/web/3.0.00/src/app/services/login.service.ts
+++ frontend/web/3.0.00/src/app/services/login.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject  } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable({
@@ -10,7 +10,9 @@ import { tap } from 'rxjs/operators';
 export class LoginService {
 
   private url: string = 'http://localhost:8080/api/v1/user/login';
-  loggedIn: boolean = false;
+
+  private loggedIn = new BehaviorSubject<boolean>(false);
+  private currentUser = new BehaviorSubject<User | null>(null);
 
   constructor(private http: HttpClient) { }
 
