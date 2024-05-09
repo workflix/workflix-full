647d1afd7c67d8e3eb87fcfabdfea72630e25da3
Braian <troncosobraianaxel@gmail.com>
2024-05-09 15:04:42 -0300

    [ADD] dashboard component import userService - front

--- frontend/web/3.0.00/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/web/3.0.00/src/app/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-dashboard',
