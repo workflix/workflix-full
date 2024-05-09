c754fe87f5bdaa6b65122d780ffd0cdd4c65984f
Braian <troncosobraianaxel@gmail.com>
2024-05-09 14:55:06 -0300

    [ADD] CorsConfig function addCorsMappings

--- backend/spring/src/main/java/com/tec/workflix/configuration/CorsConfig.java
+++ backend/spring/src/main/java/com/tec/workflix/configuration/CorsConfig.java
@@ -1,6 +1,16 @@
 package com.tec.workflix.configuration;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+}
 }
