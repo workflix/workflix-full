ea27a34c7ac2c965e8918b7a170d6d19e40ad0a5
Braian <troncosobraianaxel@gmail.com>
2024-05-10 16:08:05 -0300

    [FIX] model user act

--- frontend/web/3.0.00/src/app/models/user.ts
+++ frontend/web/3.0.00/src/app/models/user.ts
@@ -1,39 +1,40 @@
 export class User {
-    id: number = 0;
-    nombre: string = '';
-    apellido: string = '';
-    correo: string = '';
-    telefono: number = 0;
-    clave: string = '';
-    direccion: string = '';
-    ciudad: string = '';
-    provincia: string = '';
-    descripcion: string = '';
-    foto: string = '';
-
-    constructor(
-        id: number,
-        nombre: string,
-        apellido: string,
-        correo: string,
-        telefono: number,
-        clave: string,
-        direccion: string,
-        ciudad: string,
-        provincia: string,
-        descripcion: string,
-        foto: string) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.correo = correo;
-        this.telefono = telefono;
-        this.clave = clave;
-        this.direccion = direccion;
-        this.ciudad = ciudad;
-        this.provincia = provincia;
-        this.descripcion = descripcion;
-        this.foto = foto;
-    }
+  id: number = 0;
+  nombre: string = '';
+  apellido: string = '';
+  correo: string = '';
+  telefono: number = 0;
+  clave: string = '';
+  direccion: string = '';
+  ciudad: string = '';
+  provincia: string = '';
+  descripcion: string = '';
+  foto: string = '';
+  tipoUsuario: string = '';
+  constructor(
+      id: number,
+      nombre: string,
+      apellido: string,
+      correo: string,
+      telefono: number,
+      clave: string,
+      direccion: string,
+      ciudad: string,
+      provincia: string,
+      descripcion: string,
+      foto: string,
+      tipoUsuario: string) {
+      this.id = id;
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.correo = correo;
+      this.telefono = telefono;
+      this.clave = clave;
+      this.direccion = direccion;
+      this.ciudad = ciudad;
+      this.provincia = provincia;
+      this.descripcion = descripcion;
+      this.foto = foto;
+      this.tipoUsuario = tipoUsuario;
+  }
 }
-
