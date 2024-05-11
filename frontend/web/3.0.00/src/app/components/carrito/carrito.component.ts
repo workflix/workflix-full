6a222e9e8829363adb8d98de826597f680185352
Braian <troncosobraianaxel@gmail.com>
2024-05-11 20:08:19 -0300

    [ADD] component carrito - front

---
+++ frontend/web/3.0.00/src/app/components/carrito/carrito.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrito',
+  standalone: true,
+  imports: [],
+  templateUrl: './carrito.component.html',
+  styleUrl: './carrito.component.css'
+})
+export class CarritoComponent {
+
+}
