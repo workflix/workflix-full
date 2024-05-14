695709e5f16d340e10858d8cd5049287e632793a
Erick <105466809+erick-lpz@users.noreply.github.com>
2024-05-14 13:34:37 -0300

    [ADD] La URL del servidor y el constructor del servicio

--- frontend/web/3.0.00/src/app/services/service.service.ts
+++ frontend/web/3.0.00/src/app/services/service.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { Service } from '../models/service';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  private urlServices = 'http://localhost:8080/api/v1/services';
+  private urlServicios = 'http://localhost:8080/servicios';
+  
+  constructor(private http: HttpClient) { }
+
+  
 }
+
+
+
+
