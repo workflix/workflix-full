c6b64101254cfaf9cd4b2841665bc7cf8f4e9277
marceloedelgado <marceloedelgado@gmail.com>
2024-05-13 22:02:34 -0300

    [ADD][FIX] Arreglos varios de la site y se crea nueva redireccion al site general

--- frontend/web/3.0.00/src/app/pages/site/navbar-site/navbar-site.component.ts
+++ frontend/web/3.0.00/src/app/pages/site/navbar-site/navbar-site.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { PreguntasFrecuentesComponent } from '../../preguntas-frecuentes/preguntas-frecuentes.component';
 
 @Component({
   selector: 'app-navbar-site',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, PreguntasFrecuentesComponent],
   templateUrl: './navbar-site.component.html',
   styleUrl: './navbar-site.component.css'
 })
--- frontend/web/3.0.00/src/app/pages/site/site.component.ts
+++ frontend/web/3.0.00/src/app/pages/site/site.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { NavbarSiteComponent } from './navbar-site/navbar-site.component';
 import { FooterSiteComponent } from './footer-site/footer-site.component';
 import { QuienesSomosComponent } from '../quienes-somos/quienes-somos.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-site',
   standalone: true,
-  imports: [NavbarSiteComponent, FooterSiteComponent, QuienesSomosComponent],
+  imports: [RouterModule, NavbarSiteComponent, FooterSiteComponent, QuienesSomosComponent, HomeComponent],
   templateUrl: './site.component.html',
   styleUrl: './site.component.css'
 })
