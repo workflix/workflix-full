f39a22ad69f58aa83efd51b2e2a55eb10759efaa
Erick <105466809+erick-lpz@users.noreply.github.com>
2024-05-14 12:23:51 -0300

    Se creo el componente de servicios en el dashboard

---
+++ frontend/web/3.0.00/src/app/pages/dashboard/service-create/service-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './service-create.component.html',
+  styleUrl: './service-create.component.css'
+})
+export class ServiceCreateComponent {
+
+}
