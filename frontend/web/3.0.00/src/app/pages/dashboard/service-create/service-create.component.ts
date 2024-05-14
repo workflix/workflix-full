f950200681182b817cd109e0d97c692597f383b0
Erick <105466809+erick-lpz@users.noreply.github.com>
2024-05-14 14:13:23 -0300

    [ADD] Los imports, variables y constructor en ServiceCreateComponent

--- frontend/web/3.0.00/src/app/pages/dashboard/service-create/service-create.component.ts
+++ frontend/web/3.0.00/src/app/pages/dashboard/service-create/service-create.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { ServiceService } from '../../../services/service.service';
+
 
 @Component({
   selector: 'app-service-create',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './service-create.component.html',
   styleUrl: './service-create.component.css'
 })
 export class ServiceCreateComponent {
 
+  serviceArray : any[] = [];
+  error: string = '';
+  
+  nombre : string ="";
+
+  constructor(private http: HttpClient, private router: Router, private serviceService: ServiceService)
+  {
+
+  }
+
+  onSubmit(nombre: string): void  {
+    if (!nombre) {
+      this.error = 'Por favor ingresa el nombre del servicio';
+      return;
+    }
+
+    }
 }
