9dbfe0c9293f328d8797ddfcf2030baf1fa431cd
Erick <105466809+erick-lpz@users.noreply.github.com>
2024-05-14 14:33:21 -0300

    [ADD] Funcion onSubmitUpdate: Actualiza servicio y maneja errores.

--- frontend/web/3.0.00/src/app/pages/dashboard/service-create/service-create.component.ts
+++ frontend/web/3.0.00/src/app/pages/dashboard/service-create/service-create.component.ts
@@ -18,6 +18,8 @@ export class ServiceCreateComponent {
   error: string = '';
   
   nombre : string ="";
+  editingMode: number = 0;
+  currentServiceId = "";
 
   constructor(private http: HttpClient, private router: Router, private serviceService: ServiceService)
   {
@@ -40,5 +42,27 @@ export class ServiceCreateComponent {
       this.error = 'Debe completar todos los campos';
     }
   }
+
+  onSubmitUpdate(nombre: string): void {
+    if (nombre) {
+      const bodyData = {
+        nombre: nombre,
+      };
+
+      this.serviceService.updateService(+this.currentServiceId, bodyData).subscribe(
+        response => {
+          console.log ('Actualización exitosa:', response);
+          this.clearFieldsService();
+          this.router.navigate(['/dashboard']);
+        },
+        error => {
+          console.error('No se pudo actualizar correctamente:', error);
+        }
+      );
+    }else {
+      this.error = 'Debe completar todos los campos';
+    }
+ }
+
  
 }
