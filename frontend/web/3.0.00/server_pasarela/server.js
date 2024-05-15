b00dc59a6dd05165d56ceddb15b59e78a21207d6
Braian <troncosobraianaxel@gmail.com>
2024-05-15 17:39:32 -0300

    [ADD] const bodyParser

--- frontend/web/3.0.00/server_pasarela/server.js
+++ frontend/web/3.0.00/server_pasarela/server.js
@@ -2,3 +2,4 @@
 const stripe = require('stripe')('sk_test_51PGmuUByU9zDfoe2ehqFj0Cv5VFaiH6v2vi9efUAjU08Drswd9NEFHh9EdqgGXYuEiLIavpkz81Xc2kah29XBhcf00D6mXXxfm');
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
