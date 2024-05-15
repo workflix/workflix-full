892797ef5be2dba6aefb7a5fc0fcbe4444adbf79
Braian <troncosobraianaxel@gmail.com>
2024-05-15 17:38:22 -0300

    [ADD] const express

--- frontend/web/3.0.00/server_pasarela/server.js
+++ frontend/web/3.0.00/server_pasarela/server.js
@@ -1,2 +1,3 @@
 // This is your test secret API key.
 const stripe = require('stripe')('sk_test_51PGmuUByU9zDfoe2ehqFj0Cv5VFaiH6v2vi9efUAjU08Drswd9NEFHh9EdqgGXYuEiLIavpkz81Xc2kah29XBhcf00D6mXXxfm');
+const express = require('express');
