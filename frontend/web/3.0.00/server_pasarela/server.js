0f349884879147808d8b175881dd57d61e6323b8
Braian <troncosobraianaxel@gmail.com>
2024-05-15 17:38:00 -0300

    [ADD] const stripe - api

---
+++ frontend/web/3.0.00/server_pasarela/server.js
@@ -0,0 +1,2 @@
+// This is your test secret API key.
+const stripe = require('stripe')('sk_test_51PGmuUByU9zDfoe2ehqFj0Cv5VFaiH6v2vi9efUAjU08Drswd9NEFHh9EdqgGXYuEiLIavpkz81Xc2kah29XBhcf00D6mXXxfm');
