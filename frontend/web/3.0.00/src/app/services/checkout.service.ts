34c41ab3230208ebec8a3559ceb2a29a638559e4
Braian <troncosobraianaxel@gmail.com>
2024-05-15 17:47:01 -0300

    [ADD] metodo onPRoceedToPay ok en checkout services

--- frontend/web/3.0.00/src/app/services/checkout.service.ts
+++ frontend/web/3.0.00/src/app/services/checkout.service.ts
@@ -10,4 +10,18 @@ export class CheckoutService {
   private readonly _http = inject(HttpClient);
   private readonly _url = environment.serverURL;
 
+  onProceedToPay(items: User[]) {
+    console.log('users recibidos: '+items)
+    return this._http
+      .post(`${this._url}/checkout`, { items: items })
+      .pipe(
+        map(async (res: any) => {
+          const stripe = await loadStripe(environment.stripeAPIKey);
+          stripe?.redirectToCheckout({ sessionId: res.id });
+        })
+      )
+      .subscribe({
+        error: (err) => console.error('Error', err),
+      });
+  }
 }
