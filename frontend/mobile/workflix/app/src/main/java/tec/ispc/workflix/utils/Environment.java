f44f282f29175ad2c85b0c779613bbc8681fe02c
Braian <troncosobraianaxel@gmail.com>
2024-05-20 15:38:29 -0300

    [ADD] class Enviroment - login url

--- frontend/mobile/workflix/app/src/main/java/tec/ispc/workflix/utils/Environment.java
+++ frontend/mobile/workflix/app/src/main/java/tec/ispc/workflix/utils/Environment.java
@@ -4,6 +4,7 @@ public class Environment {
     public static final String URL="http://192.168.0.125:8080";
     public static final String URL_SERVICIOS=URL+"/servicios/";
     public static final String URL_USUARIOS=URL+"/usuarios/";
+    public static final String URL_LOGIN=URL+"/api/v1/user/login";
 
 
 }
