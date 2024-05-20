c500f010d300f74c1d4b5b6441c02c9f194cf92e
Braian <troncosobraianaxel@gmail.com>
2024-05-19 23:54:05 -0300

    [DEL] codigo inecesario

--- frontend/mobile/workflix/app/src/main/java/tec/ispc/workflix/models/Usuario.java
+++ frontend/mobile/workflix/app/src/main/java/tec/ispc/workflix/models/Usuario.java
@@ -68,29 +68,6 @@ public class Usuario {
         this.descripcion = descripcion;
         this.profesion = profesion;
     }
-   /* public Usuario(int id, String nombre, String apellido, String clave, String telefono, String correo, boolean is_admin){
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.clave = clave;
-        this.telefono = telefono;
-        this.correo = correo;
-        this.is_admin = is_admin;
-    }*/
-   /* public Usuario(int id, String nombre, String apellido, String clave, String telefono, String correo, boolean is_admin, String ciudad, String provincia, String descripcion, String foto, String profesion) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.clave = clave;
-        this.telefono = telefono;
-        this.correo = correo;
-        this.is_admin = is_admin;
-        this.ciudad = ciudad;
-        this.provincia = provincia;
-        this.descripcion = descripcion;
-        this.foto = foto;
-        this.profesion = profesion;
-    }*/
 
     public int getId() {
         return id;
