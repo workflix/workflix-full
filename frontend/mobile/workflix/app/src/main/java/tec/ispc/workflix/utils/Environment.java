8cc70308fe6f1889bac4b04f3d606ef91b020499
Braian <troncosobraianaxel@gmail.com>
2024-05-20 15:32:07 -0300

    [ADD] class Enviroment - ruta Servicios

--- frontend/mobile/workflix/app/src/main/java/tec/ispc/workflix/utils/Environment.java
+++ frontend/mobile/workflix/app/src/main/java/tec/ispc/workflix/utils/Environment.java
@@ -2,7 +2,7 @@ package tec.ispc.workflix.utils;
 
 public class Environment {
     public static final String URL="http://192.168.0.125:8080";
-
+    public static final String URL_SERVICIOS=URL+"/servicios/";
 
 
 }
