bcddd083a4a654c84f2b122d5f939951fe627d58
Mariela Gómez <marielagomez_95@hotmail.com>
2024-05-23 21:50:47 -0300

    agregando componente destacados

---
+++ frontend/web/3.0.00/src/app/pages/destacados/destacados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destacados',
+  standalone: true,
+  imports: [],
+  templateUrl: './destacados.component.html',
+  styleUrl: './destacados.component.css'
+})
+export class DestacadosComponent {
+
+}
