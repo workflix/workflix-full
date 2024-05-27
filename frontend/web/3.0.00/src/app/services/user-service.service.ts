53fb8316aeff677d19d2fa457d3f52d71478dd21
Braian <troncosobraianaxel@gmail.com>
2024-05-26 22:17:21 -0300

    [ADD] user-service url and array

--- frontend/web/3.0.00/src/app/services/user-service.service.ts
+++ frontend/web/3.0.00/src/app/services/user-service.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
-
+  private url:string='http://localhost:8080/prestaciones';
+  usersServices: UserService[] = [];
   constructor() { }
 }
