de9be8a0bfe35ece9d6266f7b753dd147e6b7efe
Braian <troncosobraianaxel@gmail.com>
2024-05-26 22:18:55 -0300

    [ADD] method GET and imports inuser-service

--- frontend/web/3.0.00/src/app/services/user-service.service.ts
+++ frontend/web/3.0.00/src/app/services/user-service.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError  } from 'rxjs';
 import { UserService } from './user.service';
 
 @Injectable({
@@ -7,5 +9,10 @@ import { UserService } from './user.service';
 export class UserServiceService {
   private url:string='http://localhost:8080/prestaciones';
   usersServices: UserService[] = [];
-  constructor() { }
+
+  constructor(private http:HttpClient) { }
+
+  getAllUsers():Observable<UserService[]>{
+    return this.http.get<UserService[]>(this.url+'/listar');
+  }
 }
