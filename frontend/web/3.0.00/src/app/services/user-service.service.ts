cedcf6f653a55f8af585276bb83a3b792e34daa9
Braian <troncosobraianaxel@gmail.com>
2024-05-26 22:38:33 -0300

    [ADD] Enviroment route and inyect in user-service.service.ts

--- frontend/web/3.0.00/src/app/services/user-service.service.ts
+++ frontend/web/3.0.00/src/app/services/user-service.service.ts
@@ -1,18 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError  } from 'rxjs';
+import { Observable } from 'rxjs';
 import { UserService } from './user.service';
+import { Enviroment } from '../envs/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
-  private url:string='http://localhost:8080/prestaciones';
-  usersServices: UserService[] = [];
+
+  private url:string= Enviroment.URL_USUARIO_SERVICIO;
 
   constructor(private http:HttpClient) { }
 
-  getAllUsers():Observable<UserService[]>{
+  getAllUsersServices():Observable<UserService[]>{
     return this.http.get<UserService[]>(this.url+'/listar');
   }
 }
