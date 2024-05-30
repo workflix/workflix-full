aae3d12ba820bed8bfcdca2005e4e6e1754fd0b1
Braian <troncosobraianaxel@gmail.com>
2024-05-30 17:47:57 -0300

    [ADD] logic on chat service

--- frontend/web/3.0.00/src/app/services/chat.service.ts
+++ frontend/web/3.0.00/src/app/services/chat.service.ts
@@ -4,6 +4,42 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ChatService {
+  audioFile = new Audio(
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/success.mp3"
+  );
+  constructor() {}
 
-  constructor() { }
+  conversation = new Subject<Message[]>();
+
+  messageMap = {
+    Hi: "Hello",
+    "Who are you": "My name is Agular Bot",
+    "What is Angular": "Angular is the best framework ever",
+    default: "I can't understand. Can you please repeat"
+  };
+
+  getBotAnswer(msg: string) {
+    const userMessage = new Message("user", msg);
+    this.conversation.next([userMessage]);
+    const botMessage = new Message("bot", this.getBotMessage(msg));
+
+    setTimeout(() => {
+      this.playFile();
+      this.conversation.next([botMessage]);
+    }, 1500);
+  }
+
+  playFile() {
+    this.audioFile.play();
+  }
+
+  playAudio() {
+    this.playFile("https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/success.mp3");
+  }
+
+  getBotMessage(question: string) {
+    let answer = this.messageMap[question];
+    return answer || this.messageMap["default"];
+  }
 }
+
