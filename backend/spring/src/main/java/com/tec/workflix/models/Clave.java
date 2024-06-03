13eec2d804544c69202b0aac9a6fc2801752a78e
Erick <105466809+erick-lpz@users.noreply.github.com>
2024-06-03 19:56:47 -0300

    [ADD] Clave model class with fields and getters/setters

--- backend/spring/src/main/java/com/tec/workflix/models/Clave.java
+++ backend/spring/src/main/java/com/tec/workflix/models/Clave.java
@@ -10,5 +10,33 @@ public class Clave {
     @NotBlank
     private String tokenClave;
 
+    public Clave(String clave, String confirmarClave, String tokenClave) {
+        this.clave = clave;
+        this.confirmarClave = confirmarClave;
+        this.tokenClave = tokenClave;
+    }
 
+    public @NotBlank String getConfirmarClave() {
+        return confirmarClave;
+    }
+
+    public void setConfirmarClave(@NotBlank String confirmarClave) {
+        this.confirmarClave = confirmarClave;
+    }
+
+    public @NotBlank String getTokenClave() {
+        return tokenClave;
+    }
+
+    public void setTokenClave(@NotBlank String tokenClave) {
+        this.tokenClave = tokenClave;
+    }
+
+    public @NotBlank String getClave() {
+        return clave;
+    }
+
+    public void setClave(@NotBlank String clave) {
+        this.clave = clave;
+    }
 }
