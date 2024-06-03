194804d8e156fed6754c8273d96df1e5c2fb5be7
Erick <105466809+erick-lpz@users.noreply.github.com>
2024-06-03 19:48:59 -0300

    [ADD] Email model class

---
+++ backend/spring/src/main/java/com/tec/workflix/models/Email.java
@@ -0,0 +1,10 @@
+package com.tec.workflix.models;
+
+public class Email {
+    private String remitente;
+    private String destinatario;
+    private String asunto;
+    private String usuarioNombre;
+    private String usuarioApellido;
+    private String tokenClave;
+}
