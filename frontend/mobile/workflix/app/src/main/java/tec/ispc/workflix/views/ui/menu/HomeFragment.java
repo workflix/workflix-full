a4b782efabe1837f4e4b6f1e12ea894c1c2ab22c
Braian <troncosobraianaxel@gmail.com>
2024-06-03 19:22:22 -0300

    [ADD] webview and imports on homeFragment

--- frontend/mobile/workflix/app/src/main/java/tec/ispc/workflix/views/ui/menu/HomeFragment.java
+++ frontend/mobile/workflix/app/src/main/java/tec/ispc/workflix/views/ui/menu/HomeFragment.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
 
 import tec.ispc.workflix.R;
@@ -19,7 +20,7 @@ import tec.ispc.workflix.views.ui.auth.login.LoginActivity;
 public class HomeFragment extends Fragment {
     private Button btn_home;
     private Button btn_home2;
-
+    private WebView webView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
