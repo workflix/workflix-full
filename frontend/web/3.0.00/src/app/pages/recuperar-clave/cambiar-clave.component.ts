c11d132cacf5fb784f431d024f0220c2f8b1364b
Erick <105466809+erick-lpz@users.noreply.github.com>
2024-06-03 21:45:59 -0300

    [ADD] cambiar-clave component

---
+++ frontend/web/3.0.00/src/app/pages/recuperar-clave/cambiar-clave.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cambiar-clave',
+  standalone: true,
+  imports: [],
+  templateUrl: './cambiar-clave.component.html',
+  styleUrl: './cambiar-clave.component.css'
+})
+export class CambiarClaveComponent {
+
+}
