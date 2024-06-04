810f725ee12d8dd9849c175b630e6c1d3b8a5fe3
Erick <105466809+erick-lpz@users.noreply.github.com>
2024-06-03 21:31:43 -0300

    [ADD]  functionality to send e-mails and change passwords

--- frontend/web/3.0.00/src/app/services/email-password.service.ts
+++ frontend/web/3.0.00/src/app/services/email-password.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Enviroment } from '../envs/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Email } from '../models/email';
+import { Password } from '../models/password';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailPasswordService {
+  
+  private url: string = Enviroment.URL_CLAVES;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public sendEmail(dto: Email): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post(`${this.url}/enviar-correo`, dto, { headers, responseType: 'text' });
+  }
+
+  public changePassword(dto: Password): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post(`${this.url}/cambiar-clave`, dto, { headers, responseType: 'text' });
+  }
 }
+
+
